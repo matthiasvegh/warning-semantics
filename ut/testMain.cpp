d74d9255ef8cd804db73aca6cec685fa3dc6685b
Matthias Vegh <matyas.vegh@gmail.com>
2014-07-05 10:52:08 +0200

    Added dummy unittest.

---
+++ ut/testMain.cpp
@@ -0,0 +1,2 @@
+#define BOOST_TEST_MODULE WarningSemanticsTest
+#include <boost/test/unit_test.hpp>
---
+++ ut/tests/contextStackTest.cpp
@@ -0,0 +1,9 @@
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(ContextStackTestSuite)
+
+BOOST_AUTO_TEST_CASE(compile)
+{
+}
+
+BOOST_AUTO_TEST_SUITE_END()
